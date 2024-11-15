293a1b848839e9af31536dc1fbc6ecd120b8a47c
bfl-dev <paparrasilva2@gmail.com>
2024-11-15 11:10:20 -0300

    Delete not relevant stuff

--- 06 PA/tp3/src/main/java/org/example/Enemigo.java
+++
@@ -1,7 +0,0 @@
-package org.example;
-
-class Enemigo extends Personaje {
-    public Enemigo(String nombre, int vida, int ataque, int defensa, int velocidad) {
-        super(nombre, vida, ataque, defensa, velocidad);
-    }
-}
